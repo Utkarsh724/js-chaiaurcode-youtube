df43630964209f9995964a6f4fc05067a6fce2bc
Utkarsh724 <137207305+Utkarsh724@users.noreply.github.com>
2024-12-31 05:36:27 +0000

    Learn comparison in js

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+/*console.log(2 > 1);
+console.log(2 >= 3);
+console.log(2 == 2);
+console.log(2 != 1);*/
+
+//console.log("2" > 1); //This converts the string to number first.
+//console.log(2 > "01");
+
+//console.log(null > 0); //the working of comparison and equality operators is different in js. The comparison operator converts
+//console.log(null == 0);//null to 0 while the equality operator doesnt do this. We should avoid these type of conversions.
+//console.log(null >= 0);
+
+//console.log(undefined == 0); //We should avoid these type of conversions and comparisons
+//console.log(undefined > 0);
+//console.log(undefined <= 0);
+
+// === => strict check, this also checks the datatypes
+
+console.log("2" === 2);
+
+
+
+
