0d326b440d0e1e9d2dc8633dff48148de7c0be29
Utkarsh724 <137207305+Utkarsh724@users.noreply.github.com>
2024-12-31 05:17:33 +0000

    learn operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,12 +1,12 @@
 let score = undefined
 
-console.log(typeof score)
+//console.log(typeof score)
 
 let valueInNumber = Number(score) //conversion to number data type
 
-console.log(typeof valueInNumber)
+//console.log(typeof valueInNumber)
 
-console.log(valueInNumber)
+//console.log(valueInNumber)
 
 /* Conversion to number -
 "33" => 33
@@ -19,7 +19,7 @@ undefined => NaN
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
 
 /*Conversion to boolean
 1 => true; 0 => false
@@ -30,5 +30,44 @@ console.log(booleanIsLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// *********** OPERATIONS ************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+/*console.log(2+3)
+console.log(2-3)
+console.log(2*3)
+console.log(2**3) // 2 raise to power 3
+console.log(2/3)
+console.log(2%3)*/
+
+let str1 = "hello"
+let str2 = " utkarsh"
+let str3 = str1 + str2
+//console.log(str3)
+
+//console.log("1" + 2) //This converts 2 to string
+//console.log("1" + 2 + 2); //Same as above
+//console.log(1 + 2 + "2"); //In this first we add 1 and 2 and then the result is converted to string to concatenate with string "2"
+
+//console.log(+true); //the + converts it to number, but we shouldnt do this
+//console.log(+"");
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
