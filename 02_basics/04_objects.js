31a5fbe1ddf16e2179783c73451afb532851a967
Utkarsh724 <137207305+Utkarsh724@users.noreply.github.com>
2025-01-04 14:59:00 +0000

    learn objects in js part-2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,55 @@
+//const tinderUser = new Object() //This is singleton object
+const tinderUser = {} //This is non singleton object
+
+tinderUser.id = "123abc"
+tinderUser.name = "John"
+tinderUser.isLoggedIn = false
+
+//console.log(tinderUser);
+
+const regularUser = {
+    email : "name@gmail.com",
+    fullname : {
+        userfullname : {
+            firstname : "Utkarsh",
+            lastname : "Rawat"
+        }
+    }
+}
+
+//console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+const obj3 = {5: "e", 6: "f"}
+
+//const obj3 = { obj1, obj2 } //This is a problem as there are objects inside objects in this.
+//const obj4 = Object.assign({}, obj1, obj2, obj3) //returns a concatenated object from given objects. In this the first parameter is the target object and the other parameters are concatenated into the target object. The returned object is the target object.
+
+const obj4 = {...obj1, ...obj2, ...obj3} //Spread operator('...') :- This is the best way to concatenate objects
+//console.log(obj4);
+
+const users = [
+    {
+        id: 1,
+        email: "user@gmail.com",
+    },
+    {
+        id: 1,
+        email: "user@gmail.com",
+    },
+    {
+        id: 1,
+        email: "user@gmail.com",
+    },
+]
+
+//console.log(users[1].email)
+//console.log(tinderUser);
+
+//console.log(Object.keys(tinderUser)); //Returns an array containing keys of the specified object.
+//console.log(Object.values(tinderUser)); //Returns an array containing values of the specified object.
+//console.log(Object.entries(tinderUser)); 
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn')); //Returns true if the specified object contains the specified key.
+
